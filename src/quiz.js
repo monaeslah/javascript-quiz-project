8dbf0c793e3ee3632512b9911df8122984affeb9
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-10-07 17:33:15 +0200

    day-1 is done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,39 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  constructor (questions, timeLimit, timeRemaining) {
+    this.questions = questions
+    this.timeLimit = timeLimit
+    this.timeRemaining = timeRemaining
+    this.correctAnswers = 0
+    this.currentQuestionIndex = 0
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion () {
+    return this.questions[this.currentQuestionIndex]
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion () {
+    this.currentQuestionIndex += 1
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions () {
+    for (let i = 0; i < this.questions.length - 1; i++) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i]
+      ]
+    }
+  }
 
-    // 6. hasEnded()
+  checkAnswer (answer) {
+    if (answer) {
+      this.correctAnswers += 1
+    }
+  }
+
+  hasEnded () {
+    return this.currentQuestionIndex >= this.questions.length
+  }
 }

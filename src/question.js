d18bec1114729d27193340e646c430c71108457c
Bopally <estelleb0707@gmail.com>
2024-10-07 17:36:13 +0200

    Day 1st done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,20 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty; //The difficulty will be a number between 1 and 3, with 1 being the easiest and 3 being the hardest.
+  }
 
-    // 2. shuffleChoices()
+  // 2. shuffleChoices()
+
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+    }
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,46 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
 
-    // 4. shuffleQuestions()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    // answer by default is true
+    if (answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
 }
